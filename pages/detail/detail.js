75853f18249c7bb7e1e58385819dd9369ebb2464
avaqi29 <avaqi29@users.noreply.github.com>
2018-07-16 15:14:24 -0400

    finish essential

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -5,7 +5,13 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    id:"",
+    news_title:"",
+    news_date:"",
+    readCount:"",
+    news_pic:"",
+    news_firstcontent:"",
+    news_content: []
   },
 
   /**
@@ -14,54 +20,46 @@ Page({
   onLoad: function (options) {
     var $data = options.id;
     console.log('news id=' + $data);
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
+    this.setData({
+      id : $data
+    })
+    this.getNewsDetail()
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+    this.getNewsDetail(() => {
+      wx.stopPullDownRefresh()
+    })
   },
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
+  getNewsDetail(callback) {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/detail',
+      data: {
+        id: this.data.id
+      },
+      success: res => {
+        let result = res.data.result
+        this.setNewsDetail(result)
+      },
+      complete: () => {
+        callback && callback()
+      }
+    })
   },
 
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
+  setNewsDetail(result) {
+    this.setData({
+      news_title: result.title,
+      news_date: `${result.source} ${result.date.slice(5, 10)} ${result.date.slice(11, 16)}`,
+      news_pic: result.firstImage,
+      readCount: result.readCount, 
+      news_firstcontent: result.content[0],
+      news_content: result.content.slice(1)
+    })
   }
+
 })
